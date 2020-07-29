2290a30b18a26516e09313cf3a012748fd56c98a
sahinfarzina <sahinfarzina84@gmail.com>
2020-07-29 14:12:41 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,[Validators.required, CustomValidators.invalidProjectName],CustomValidators.asyncInvalidProjectName),
+      'email':new FormControl(null,[Validators.required,Validators.email]),
+      'projectStatus': new FormControl('critical')
+    })
+  }
+  onProjectSave(){
+    console.log(this.projectForm.value);
+  }
 }
